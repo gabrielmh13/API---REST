998ccfed904a2fbf444ec675ecf33320a8023046
Gabriel Hernandes <gabrielmh13@hotmail.com>
2021-12-22 23:29:52 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false }));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
